de84a1bc2c0a664b0a851e6c4861624b948d2d9e
agahafiz04 <agahafiz04@gmail.com>
2023-08-15 13:12:10 +0700

    Initial Commit (First Version Of The Game)

---
+++ index.js
@@ -0,0 +1,93 @@
+//  Planning
+// 1. Do it have an interface? // Yes, later on
+// 2. Where input is getting from? // From player
+// 3. What is the output outcome? // Random between player and computer
+// 4. How to process the input to become the output? // Using random method
+
+//  PseudoCode / Code
+let startGameString = `--- Welcome To Rock Paper Scissors Game Made With Javascript ---
+
+1. To Play The Game Please Insert A String Between Rock, Paper, and Scissors
+2. There Will Be 5 Round To Determine The Winner
+3. Done 
+
+Enjoy The Game!
+
+`;
+
+console.log(startGameString);
+let score = 0;
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "Rock";
+  } else if (randomNumber === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+function getPlayerChoice() {
+  let playerChoice = prompt(
+    `--- Welcome To Rock Paper Scissors Game Made With Javascript ---
+
+  1. Please Insert A String Between Rock, Paper, and Scissors
+  2. There Will Be 5 Round To Determine The Winner
+  3. Done 
+
+  Enjoy The Game!
+  
+  Insert your choice below...`
+  );
+
+  return playerChoice;
+}
+
+function playGame(playerSelection, computerSelection) {
+  let equalStringPlayer =
+    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+  let equalStringComputer =
+    computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+
+  if (
+    (equalStringPlayer === "Rock" && equalStringComputer === "Scissors") ||
+    (equalStringPlayer === "Paper" && equalStringComputer === "Rock") ||
+    (equalStringPlayer === "Scissors" && equalStringComputer === "Paper")
+  ) {
+    score += 1;
+    return `You Choose ${equalStringPlayer}, The Enemy Choose ${equalStringComputer}. Congratulation you win this round!`;
+  } else if (
+    (equalStringComputer === "Rock" && equalStringPlayer === "Scissors") ||
+    (equalStringComputer === "Paper" && equalStringPlayer === "Rock") ||
+    (equalStringComputer === "Scissors" && equalStringPlayer === "Paper")
+  ) {
+    return `You Choose ${equalStringPlayer}, The Enemy Choose ${equalStringComputer}. Sorry, you lose the round`;
+  } else if (equalStringPlayer === equalStringComputer) {
+    return `You Choose ${equalStringPlayer}, The Enemy Choose ${equalStringComputer}. Its a draw round...`;
+  } else {
+    return `You Choose ${equalStringPlayer}, The Enemy Choose ${equalStringComputer}. Please enter a valid play`;
+  }
+}
+
+function gamePlay() {
+  for (let i = 0; i < 5; ++i) {
+    let playerPlay = getPlayerChoice();
+    let computerPlay = getComputerChoice();
+    let play = playGame(playerPlay, computerPlay);
+    console.log(play);
+  }
+  scoreSet();
+  return;
+}
+
+function scoreSet() {
+  if (score < 3) {
+    console.log(`You Lose, Try Again? (Total Score ${score})`);
+  } else {
+    console.log(`You win! (Total Score ${score})`);
+  }
+}
+
+gamePlay();
